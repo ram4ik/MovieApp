d8d3915ebe2a3eab5fb8bf6c7932832e3ec623b8
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-04 14:51:34 +0200

    init

--- MovieApp/ContentView.swift
+++ MovieApp/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSettings = false
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Group {
+                HomeTabView()
+            }
+            .navigationBarTitle("Movies", displayMode: .automatic)
+            .navigationBarItems(trailing: HStack {
+                settingsButton
+            })
+            .sheet(isPresented: $showSettings) {
+                Text("Settings View")
+            }
+        }
+    }
+    
+    private var settingsButton: some View {
+        Button {
+            showSettings.toggle()
+        } label: {
+            HStack {
+                Image(systemName: "gear")
+                    .imageScale(.large)
+                    .foregroundColor(.gray)
+            }.frame(width: 30, height: 30)
+        }
+
     }
 }
 
---
+++ MovieApp/Views/HomeTabView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTabView.swift
+//  MovieApp
+//
+//  Created by Ramill Ibragimov on 04.03.2021.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeTabView()
+    }
+}
