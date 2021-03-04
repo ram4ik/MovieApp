43fec1c6c27bef549e7b72cff0ed250f08625656
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-04 16:11:24 +0200

    Movies & Discovery tabs

---
+++ MovieApp/Views/DiscoverView.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoverView.swift
+//  MovieApp
+//
+//  Created by Ramill Ibragimov on 04.03.2021.
+//
+
+import SwiftUI
+
+struct DiscoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiscoverView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscoverView()
+    }
+}
---
+++ MovieApp/Views/MoviesView.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesView.swift
+//  MovieApp
+//
+//  Created by Ramill Ibragimov on 04.03.2021.
+//
+
+import SwiftUI
+
+struct MoviesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoviesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoviesView()
+    }
+}
