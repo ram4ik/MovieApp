f5943f30e0d3dbbadd1597affd864122eb2cdf4d
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-04 14:32:50 +0200

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Ramill Ibragimov on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
