4a38fc52d95fb77b6cc08cd9e5427c84136a6938
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-03-05 09:44:44 +0200

    AsyncImage

---
+++ MovieApp/AsyncImage/AsyncImage.swift
@@ -0,0 +1,38 @@
+//
+//  AsyncImage.swift
+//  MovieApp
+//
+//  Created by ramil on 05.03.2021.
+//
+
+import SwiftUI
+
+struct AsyncImage<Placeholder: View>: View {
+    @StateObject private var loader: ImageLoader
+    private let placeholder: Placeholder
+    private let image: (UIImage) -> Image
+    
+    init(
+        url: URL,
+        @ViewBuilder placeholder: () -> Placeholder,
+        @ViewBuilder image: @escaping (UIImage) -> Image = Image.init(uiImage:)
+        ) {
+        self.placeholder = placeholder()
+        self.image = image
+        _loader = StateObject(wrappedValue: ImageLoader(url: url, cache: Environment(\.imageCache).wrappedValue))
+    }
+    
+    var body: some View {
+        content.onAppear(perform: loader.load)
+    }
+    
+    private var content: some View {
+        Group {
+            if loader.image != nil {
+                image(loader.image!)
+            } else {
+                placeholder
+            }
+        }
+    }
+}
---
+++ MovieApp/AsyncImage/EnvironmentValues+ImageCache.swift
@@ -0,0 +1,19 @@
+//
+//  EnvironmentValues+ImageCache.swift
+//  MovieApp
+//
+//  Created by ramil on 05.03.2021.
+//
+
+import SwiftUI
+
+struct ImageCacheKey: EnvironmentKey {
+    static let defaultValue: ImageCache = TempImageCache()
+}
+
+extension EnvironmentValues {
+    var imageCache: ImageCache {
+        get { self[ImageCacheKey.self] }
+        set { self[ImageCacheKey.self] = newValue }
+    }
+}
