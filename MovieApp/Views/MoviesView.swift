89a9e28b35f1012457543751ce909d7b7794dd08
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-03-05 11:22:35 +0200

    Movies View

--- MovieApp/Views/MoviesView.swift
+++ MovieApp/Views/MoviesView.swift
@@ -8,8 +8,76 @@
 import SwiftUI
 
 struct MoviesView: View {
+    @State private var searchTerm = ""
+    @State private var selectionIndex = 0
+    @State private var tabs = ["Now Playing", "Upcoming", "Trending"]
+    
+    @ObservedObject var movieManager = MovieDownloadManager()
+    
+    init() {
+        UITableView.appearance().backgroundColor = UIColor.clear
+        UITableViewCell.appearance().selectionStyle = .none
+        
+        UINavigationBar.appearance().backgroundColor = .clear
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().barTintColor = .orange
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.orange]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.orange]
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        UINavigationBar.appearance().shadowImage = UIImage()
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: .leading) {
+                Text(tabs[selectionIndex])
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.red)
+                    .padding(.top)
+                
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .imageScale(.medium)
+                    TextField("Search...", text: $searchTerm)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+            }.padding(.horizontal)
+            
+            VStack {
+                Picker("_", selection: $selectionIndex) {
+                    ForEach(0..<tabs.count) { index in
+                        Text(tabs[index])
+                            .font(.title)
+                            .bold()
+                            .tag(index)
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+                .onChange(of: selectionIndex, perform: { value in
+                    if selectionIndex == 0 {
+                        movieManager.getNowPlaying()
+                    } else if selectionIndex == 1 {
+                        movieManager.getUpcoming()
+                    } else if selectionIndex == 2 {
+                        movieManager.getPopular()
+                    }
+                })
+            }.padding()
+            
+            List {
+                ForEach(movieManager.movies.filter {
+                            searchTerm.isEmpty ? true : $0.title?.lowercased().localizedStandardContains(searchTerm.lowercased()) ?? true }) { movie in
+                    NavigationLink(
+                        destination: Text(movie.titleWithLanguage),
+                        label: {
+                            Text(movie.titleWithLanguage)
+                        }).listRowBackground(Color.clear)
+                }
+            }.onAppear() {
+                movieManager.getNowPlaying()
+            }
+            Spacer()
+        }
     }
 }
 
